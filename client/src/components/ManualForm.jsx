9972d087c9ac9576009ce0efbae3bf279d4c645c
WarrenMfg <warrenmfg@icloud.com>
2020-04-16 09:07:42 -0400

    line chart front end complete

--- client/src/components/ManualForm.jsx
+++ client/src/components/ManualForm.jsx
@@ -8,11 +8,37 @@ class ManualForm extends React.Component {
       inputCount: 1
     };
 
-    this.addMoreData = this.addMoreData.bind(this);
+    this.handleCancel = this.handleCancel.bind(this);
+    this.addMorePieData = this.addMorePieData.bind(this);
     this.handleSubmitPieForm = this.handleSubmitPieForm.bind(this);
+    this.addMoreLineData = this.addMoreLineData.bind(this);
+    this.addMoreLines = this.addMoreLines.bind(this);
+    this.handleSubmitLineForm = this.handleSubmitLineForm.bind(this);
   }
 
-  addMoreData(e) {
+  handleCancel() {
+    this.props.handleHideManualForm();
+  }
+
+  // PIE
+  makePie() {
+    return (
+      <div className="ManualForm">
+        <form>
+          <p>Title:&nbsp;<input type="text" name="title" autoFocus/></p>
+          <p>Label:&nbsp;<input type="text" name="label0"/>&nbsp;Data:&nbsp;<input type="text" name="data0"/></p>
+
+          <p>
+            <button type="button" onClick={this.addMorePieData}>Add More Data</button>
+            <button type="button" onClick={this.handleSubmitPieForm}>Submit</button>
+            <button type="button" onClick={this.handleCancel}>Cancel</button>
+          </p>
+        </form>
+      </div>
+    );
+  }
+
+  addMorePieData(e) {
     const clone = e.target.parentElement.previousElementSibling.cloneNode(true);
 
     clone.querySelectorAll('input').forEach((el, i) => {
@@ -32,12 +58,11 @@ class ManualForm extends React.Component {
   handleSubmitPieForm() {
     const data = {
       title: '',
-      labels: [],
-      data: []
+      data: [],
+      labels: []
     };
     const allInputs = document.querySelectorAll('input');
     data.title = allInputs[0].value;
-    allInputs[0].value = '';
 
     allInputs.forEach((input, i) => {
       if (i === 0) {
@@ -45,10 +70,8 @@ class ManualForm extends React.Component {
       }
       if (i % 2 === 0) {
         data.data.push(input.value);
-        input.value = '';
       } else {
         data.labels.push(input.value);
-        input.value = '';
       }
     });
 
@@ -56,18 +79,96 @@ class ManualForm extends React.Component {
     this.props.POST(data, 'manually', 'pie');
   }
 
-  makePie() {
+  // LINE
+  makeLine() {
     return (
       <div className="ManualForm">
-        <form /*action={ `/api/manually/${this.props.displayData}` } method="POST" onsubmit="(event) => event.preventDefault()" */ >
+        <form>
           <p>Title:&nbsp;<input type="text" name="title" autoFocus/></p>
-          <p>Label:&nbsp;<input type="text" name="label0"/>&nbsp;Data:&nbsp;<input type="text" name="data0"/></p>
+          <div className="ManualForm-line">
+            <p>Line Name:&nbsp;<input type="text" name="lineName0"/></p>
+            <p>Label:&nbsp;<input type="text" name="label0"/>&nbsp;Data:&nbsp;<input type="text" name="data0"/></p>
+          </div>
 
-          <p><button type="button" onClick={this.addMoreData}>Add More Data</button></p>
-          <p><button type="button" onClick={this.handleSubmitPieForm}>Submit</button></p>
+          <p>
+            <button type="button" onClick={this.addMoreLineData}>Add More Data</button>
+            <button type="button" onClick={this.addMoreLines}>Add More Lines</button>
+            <button type="button" onClick={this.handleSubmitLineForm}>Submit</button>
+          </p>
         </form>
       </div>
     );
+  }
+
+  addMoreLineData() {
+    const parents = document.querySelectorAll('.ManualForm-line');
+    const clones = Array.from(parents).map(div => div.lastElementChild.cloneNode(true));
+
+    parents.forEach((parent, i) => {
+      clones[i].children[0].value = '';
+      clones[i].children[1].value = '';
+      clones[i].children[0].name = `label${this.state.inputCount}`;
+      clones[i].children[1].name = `data${this.state.inputCount}`;
+      parent.append(clones[i]);
+    });
+
+    this.setState({ inputCount: this.state.inputCount + 1 });
+  }
+
+  addMoreLines() {
+    // get last parent
+    const parents = Array.from(document.querySelectorAll('.ManualForm-line'));
+    const lastParentClone = parents[parents.length - 1].cloneNode(true);
+    
+    // remove values from inputs
+    lastParentClone.querySelectorAll('input').forEach(input => input.value = '');
+    
+    // update name attribute for Line Name
+    let lineName = lastParentClone.firstElementChild.firstElementChild.name;
+    lineName = `lineName${+lineName.split('lineName')[1] + 1}`;
+    lastParentClone.firstElementChild.firstElementChild.name = lineName;
+    
+    // insert into DOM
+    const lastChild = document.querySelector('form').lastElementChild;
+    lastChild.before(lastParentClone);
+  }
+
+  handleSubmitLineForm() {
+    const data = {
+      title: document.querySelector('input[name="title"]').value,
+      datasets: [],
+      labels: []
+    };
+
+    const lines = document.querySelectorAll('.ManualForm-line');
+
+    let dataset = {
+      label: '',
+      data: []
+    }
+    lines.forEach(line => {
+      const inputs = line.querySelectorAll('input');
+      inputs.forEach((input, j) => {
+        if (j === 0) {
+          dataset.label = input.value;
+        } else if (j % 2 === 0) {
+          dataset.data.push(input.value)
+        } else {
+          data.labels.push(input.value);
+        }
+      });
+
+      data.datasets.push(dataset);
+      dataset = {
+        label: '',
+        data: []
+      };
+    });
+
+
+    this.props.handleHideManualForm();
+    // this.props.POST(data, 'manually', 'line');
+    console.log(data);
   }
 
 
@@ -75,6 +176,8 @@ class ManualForm extends React.Component {
     const type = this.props.displayData;
     if (type === 'pie') {
       return this.makePie();
+    } else if (type === 'line') {
+      return this.makeLine();
     }
   }
 }
